7d24f2cb2f31feb5db61eec48d59b4290dc0b6d4
Apple <784913337@qq.com>
2016-05-17 11:14:57 +0800

    第一次成功上传

---
+++ LeanChat/LeanChat/lhScanQCodeViewController.h
@@ -0,0 +1,13 @@
+//
+//  lhScanQCodeViewController.h
+//  lhScanQCodeTest
+//
+//  Created by hejiyang on 16/5/10.
+//  Copyright © 2016年 ichronocloud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface lhScanQCodeViewController : UIViewController
+
+@end
---
+++ LeanChat/LeanChat/lhScanQCodeViewController.m
@@ -0,0 +1,166 @@
+//
+//  lhScanQCodeViewController.m
+//  lhScanQCodeTest
+//
+//  Created by hejiyang on 16/5/10.
+//  Copyright © 2016年 ichronocloud. All rights reserved.
+//
+
+#import "lhScanQCodeViewController.h"
+#import "QRCodeReaderView.h"
+
+#import <AVFoundation/AVFoundation.h>
+#import <AudioToolbox/AudioToolbox.h>
+
+#define DeviceMaxHeight ([UIScreen mainScreen].bounds.size.height)
+#define DeviceMaxWidth ([UIScreen mainScreen].bounds.size.width)
+#define widthRate DeviceMaxWidth/320
+#define IOS8 ([[UIDevice currentDevice].systemVersion intValue] >= 8 ? YES : NO)
+
+@interface lhScanQCodeViewController ()<QRCodeReaderViewDelegate,AVCaptureMetadataOutputObjectsDelegate,UINavigationControllerDelegate,UIAlertViewDelegate>
+{
+    QRCodeReaderView * readview;//二维码扫描对象
+    
+    BOOL isFirst;//第一次进入该页面
+    BOOL isPush;//跳转到下一级页面
+}
+
+@property (strong, nonatomic) CIDetector *detector;
+
+@end
+
+@implementation lhScanQCodeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.title = @"扫描";
+    self.view.backgroundColor = [UIColor whiteColor];
+    UIBarButtonItem * lbbItem = [[UIBarButtonItem alloc]initWithTitle:@"返回" style:UIBarButtonItemStyleDone target:self action:@selector(backButtonEvent)];
+    self.navigationItem.leftBarButtonItem = lbbItem;
+    
+    isFirst = YES;
+    isPush = NO;
+    
+    [self InitScan];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - 返回
+- (void)backButtonEvent
+{
+    [self dismissViewControllerAnimated:YES completion:^{
+        
+    }];
+}
+
+#pragma mark 初始化扫描
+- (void)InitScan
+{
+    if (readview) {
+        [readview removeFromSuperview];
+        readview = nil;
+    }
+    
+    readview = [[QRCodeReaderView alloc]initWithFrame:CGRectMake(0, 0, DeviceMaxWidth, DeviceMaxHeight)];
+    readview.is_AnmotionFinished = YES;
+    readview.backgroundColor = [UIColor clearColor];
+    readview.delegate = self;
+    readview.alpha = 0;
+    
+    [self.view addSubview:readview];
+    
+    [UIView animateWithDuration:0.5 animations:^{
+        readview.alpha = 1;
+    }completion:^(BOOL finished) {
+
+    }];
+    
+}
+
+#pragma mark -QRCodeReaderViewDelegate
+- (void)readerScanResult:(NSString *)result
+{
+    readview.is_Anmotion = YES;
+    [readview stop];
+    
+    //播放扫描二维码的声音
+    SystemSoundID soundID;
+    NSString *strSoundFile = [[NSBundle mainBundle] pathForResource:@"noticeMusic" ofType:@"wav"];
+    AudioServicesCreateSystemSoundID((__bridge CFURLRef)[NSURL fileURLWithPath:strSoundFile],&soundID);
+    AudioServicesPlaySystemSound(soundID);
+    
+    [self accordingQcode:result];
+    
+    [self performSelector:@selector(reStartScan) withObject:nil afterDelay:1.5];
+}
+
+#pragma mark - 扫描结果处理
+- (void)accordingQcode:(NSString *)str
+{
+    UIAlertView * alertView = [[UIAlertView alloc]initWithTitle:@"扫描结果" message:str delegate:nil cancelButtonTitle:@"确定" otherButtonTitles:nil, nil];
+    [alertView show];
+}
+
+- (void)reStartScan
+{
+    readview.is_Anmotion = NO;
+    
+    if (readview.is_AnmotionFinished) {
+        [readview loopDrawLine];
+    }
+    
+    [readview start];
+}
+
+#pragma mark - view
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    
+    if (isFirst || isPush) {
+        if (readview) {
+            [self reStartScan];
+        }
+    }
+    
+}
+
+- (void)viewDidDisappear:(BOOL)animated
+{
+    [super viewDidDisappear:animated];
+    
+    if (readview) {
+        [readview stop];
+        readview.is_Anmotion = YES;
+    }
+    
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    
+    if (isFirst) {
+        isFirst = NO;
+    }
+    if (isPush) {
+        isPush = NO;
+    }
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
