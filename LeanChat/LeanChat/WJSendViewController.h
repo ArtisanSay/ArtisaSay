ba6e8a4e0f82eb9d6b52ecb95e966e86903c48d8
Apple <784913337@qq.com>
2016-05-17 09:51:52 +0800

    成功的第一次提交

---
+++ LeanChat/LeanChat/WJSendViewController.h
@@ -0,0 +1,15 @@
+//
+//  WJSendViewController.h
+//  ArtisaSay
+//
+//  Created by Apple on 16/5/10.
+//  Copyright © 2016年 YiJiangTianCheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WJSendViewController : UIViewController
+- (IBAction)dismissBtn:(id)sender;
+- (IBAction)sendBtn:(id)sender;
+
+@end
